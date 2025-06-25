ccc6372be95b24a642a292315e22f4b2a1c4137d
vatsal0 <vatsalbaherwani@gmail.com>
2025-06-25 04:54:21 +0000

    setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+source ~/miniconda3/bin/activate
+conda init bash
+
+conda create -n nanogcg python=3.11
+conda activate nanogcg
+
+cd nanoGCG
+pip install -e .
